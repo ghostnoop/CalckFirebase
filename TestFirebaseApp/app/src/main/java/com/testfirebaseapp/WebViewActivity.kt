a7dbc82719594f970300840df3e29e93b7775e55
Gilyazov Marat <giliyazovmarat@gmail.com>
2020-06-04 09:03:51 +0300

    Update WebViewActivity.kt

--- TestFirebaseApp/app/src/main/java/com/testfirebaseapp/WebViewActivity.kt
+++ TestFirebaseApp/app/src/main/java/com/testfirebaseapp/WebViewActivity.kt
@@ -1,26 +1,22 @@
 package com.testfirebaseapp
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.view.View
-import android.webkit.*
+import android.util.Log
+import android.webkit.ValueCallback
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.testfirebaseapp.web.CustomWebViewClient
 import kotlinx.android.synthetic.main.activity_web_view.*
-import java.io.File
-import java.text.SimpleDateFormat
-import java.util.*
 
 
 class WebViewActivity : AppCompatActivity() {
-
-
 
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -31,21 +27,18 @@ class WebViewActivity : AppCompatActivity() {
         webView.webViewClient = CustomWebViewClient()
 
 
-
         val url = intent.extras!!.getString("url")
 
         webView.settings.domStorageEnabled = true
         webView.settings.javaScriptEnabled = true
-        webView.settings.setSupportZoom(false)
-        webView.settings.allowFileAccess = true
         webView.settings.allowFileAccess = true
         webView.settings.allowContentAccess = true
 
         webView.loadUrl(url)
 
-
+        Log.e("N@@", Build.VERSION.SDK_INT.toString())
         webView.webChromeClient = object : WebChromeClient() {
-            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+
             override fun onShowFileChooser(
                 view: WebView,
                 filePath: ValueCallback<Array<Uri>>,
@@ -56,16 +49,29 @@ class WebViewActivity : AppCompatActivity() {
                     message = null
                 }
                 messageArray = filePath
-                val intent = fileChooserParams.createIntent()
-                intent.type = "image/*"
-                try {
-                    startActivityForResult(intent, REQUEST_SELECT_FILE)
-                } catch (e: Exception) {
-                    message = null
-                    return false
+                Log.e("N@@", Build.VERSION.SDK_INT.toString())
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    val intent = fileChooserParams.createIntent()
+                    intent.type = "image/*"
+                    try {
+                        startActivityForResult(intent, REQUEST_SELECT_FILE)
+                    } catch (e: Exception) {
+                        message = null
+                        return false
+                    }
+                    return true
+                } else {
+                    val intent = Intent(Intent.ACTION_GET_CONTENT)
+                    intent.addCategory(Intent.CATEGORY_OPENABLE)
+                    intent.type = "image/*"
+                    startActivityForResult(
+                        Intent.createChooser(intent, "Chose"),
+                        FILECHOOSER_RESULTCODE
+                    )
+                    return true
                 }
-                return true
             }
+
 
             fun openFileChooser(uploadMsg: ValueCallback<Uri>, acceptType: String = "") {
                 message = uploadMsg
@@ -81,6 +87,7 @@ class WebViewActivity : AppCompatActivity() {
 
 
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             if (requestCode == REQUEST_SELECT_FILE) {
@@ -94,6 +101,7 @@ class WebViewActivity : AppCompatActivity() {
                 )
                 messageArray = null
             }
+            Toast.makeText(this, "$requestCode $FILECHOOSER_RESULTCODE", Toast.LENGTH_SHORT).show()
         } else if (requestCode == FILECHOOSER_RESULTCODE) {
             if (null == message)
                 return
@@ -111,7 +119,6 @@ class WebViewActivity : AppCompatActivity() {
         val REQUEST_SELECT_FILE = 100
         val FILECHOOSER_RESULTCODE = 1
     }
-
 
 
     override fun onBackPressed() {
